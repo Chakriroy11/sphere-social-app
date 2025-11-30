39d7a74b8f776b8639fbed8fa34da322bf2d7447
Chandaka Chakri <chandakachakri06@gmail.com>
2025-11-30 21:50:35 +0530

    Add Cloudinary config

---
+++ backend/config/cloudinary.js
@@ -0,0 +1,20 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+require('dotenv').config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'sphere_social',
+        allowed_formats: ['jpg', 'png', 'jpeg', 'mp4', 'mov'],
+        resource_type: 'auto' // Automatically detects images vs videos
+    },
+});
+
+module.exports = { cloudinary, storage };
